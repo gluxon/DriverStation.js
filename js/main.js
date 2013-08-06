874b809fe9bcf2713962317995dfb311772d2946
gluxon <bcheng.gt@gmail.com>
2013-08-06 09:04:24 -0400

    Settings no longer contained in yaml files

--- js/main.js
+++ js/main.js
@@ -4,21 +4,8 @@ var win = gui.Window.get();
 var driverstation = require('driverstation');
 
 var fs = require('fs');
-require('js-yaml');
-
-var SETTINGS_FILE = './config/settings.yaml';
-
-// Check if settings file exists
-fs.exists(SETTINGS_FILE, function(exists) {
-  if (!exists) {
-    alert("Settings file could not be loaded. Closing.");
-    process.exit(0);
-  }
-});
 
 onload = function() {
-  var settings = require(SETTINGS_FILE);
-
   states = new States();
   dstimer = new DSTimer();
   hookLinks();
